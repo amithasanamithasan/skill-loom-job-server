cb8ae84720e9bb1212b6dde012d2702cbcfafe11
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-09-08 11:24:30 +0600

    initial setup node js with mongodb

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+    res.send("Skill Loom Job Portal API is running");
+});
+
+
+// Server listen
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(` Server is running on port ${PORT}`);
+});
+
