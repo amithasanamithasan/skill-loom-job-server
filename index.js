c7e8f561ac00f9460396c34da1896f5dde7f0c79
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-09-14 10:37:14 +0600

    Setup env

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://skillserver:Sl12ZKPKFsmKY1Xn@cluster0.1gieptu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1gieptu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
