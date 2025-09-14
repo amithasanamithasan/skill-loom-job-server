6700b92738d4ae666c42e7d934d0c0a3db68f41b
amithasanamithasan <amit35-1773@diu.edu.bd>
2025-09-14 10:29:23 +0600

    databseConnected setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require("dotenv");
 
 
@@ -12,6 +13,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb+srv://skillserver:Sl12ZKPKFsmKY1Xn@cluster0.1gieptu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 // Root route
 app.get("/", (req, res) => {
     res.send("Skill Loom Job Portal API is running");
